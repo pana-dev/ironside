6fc6de539260016f71e013ebb45e0c16de9ee225
Victor Marquez <victor.marquez@powerdms.com>
2017-02-16 23:00:34 -0500

    Update to a view based structure.

--- gulpfile.js
+++ gulpfile.js
@@ -3,151 +3,102 @@
 // =========================
 
 // GULP
-const	GULP = require('gulp');
+const GULP = require('gulp');
 
 // CSS
-const	SASS = require('gulp-sass');
-const	MINCSS = require('gulp-clean-css');
-const	PREFIXCSS = require('gulp-autoprefixer');
+const SASS = require('gulp-sass');
+const MINCSS = require('gulp-clean-css');
+const PREFIXCSS = require('gulp-autoprefixer');
 
 // JAVASCRIPT
-const	JSUGLIFY = require('gulp-uglify');
-const	JSCONCAT = require('gulp-concat');
+const JSUGLIFY = require('gulp-uglify');
+const JSCONCAT = require('gulp-concat');
 
 // OTHER
-const	WATCH = require('gulp-watch');
-const	RENAME = require('gulp-rename');
+const WATCH = require('gulp-watch');
+const RENAME = require('gulp-rename');
 
-// DEV COMPILE
+// Main Tasks
 ///////////////////////////////////////////////
 
 GULP.task('dev', function() {
-	GULP.watch('source/pre/sass/**/*.sass', ['sass-dev']);
-	GULP.watch('source/pre/js/**/*.js', ['js-dev']);
-	GULP.watch('source/pre/assets/**/*', ['imagemin']);
-	GULP.watch('source/pre/fonts/**/*', ['fonts']);
+    GULP.watch('source/sass/**/*.sass', ['sass-dev']);
+    GULP.watch('source/js/**/*.js', ['js-dev']);
+    GULP.watch('source/assets/**/*', ['imagemin']);
 });
 
 GULP.task('prod', function() {
-	GULP.watch('source/pre/sass/**/*.sass', ['sass-prod']);
-	GULP.watch('source/pre/js/**/*.js', ['js-prod']);
-	GULP.watch('source/pre/assets/**/*', ['imagemin-prod']);
-	GULP.watch('source/pre/fonts/**/*', ['fonts']);
-});
-
-GULP.task('wp', function() {
-	GULP.watch('source/pre/sass/**/*.sass', ['sass-wp']);
-	GULP.watch('source/pre/js/**/*.js', ['js-wp']);
-	GULP.watch('source/pre/assets/**/*', ['imagemin-wp']);
-	GULP.watch('source/pre/fonts/**/*', ['fonts-wp']);
+    GULP.watch('source/sass/**/*.sass', ['sass-prod']);
+    GULP.watch('source/js/**/*.js', ['js-prod']);
+    GULP.watch('source/assets/**/*', ['imagemin-prod']);
 });
 
 // SASS & CSS
 ///////////////////////////////////////////////
 
 GULP.task('sass-dev', function() {
-	GULP.src('source/pre/sass/**/*.sass')
-		.pipe(SASS.sync().on('error', SASS.logError))
-		.pipe(PREFIXCSS())
-		.pipe(GULP.dest('source/post/css'))
-		.pipe(GULP.dest('source/_dev/css'));
-
-	GULP.src('source/pre/css/**/*.css')
-		.pipe(GULP.dest('source/_dev/css'))
-		.pipe(GULP.dest('html/css'));
+    GULP.src('source/sass/**/*.sass')
+        .pipe(SASS.sync().on('error', SASS.logError))
+        .pipe(PREFIXCSS())
+        .pipe(GULP.dest('source/css'));
 });
 
 GULP.task('sass-prod', function() {
-	GULP.src('source/pre/sass/**/*.sass')
-		.pipe(SASS.sync().on('error', SASS.logError))
-		.pipe(PREFIXCSS())
-		.pipe(MINCSS({debug: true}, function(details) {
+    GULP.src(['source/sass/**/*.sass', '!source/sass/views/**/*.sass'])
+        .pipe(SASS.sync().on('error', SASS.logError))
+        .pipe(PREFIXCSS())
+        .pipe(MINCSS({ debug: true }, function(details) {
             console.log(details.name + ': ' + details.stats.originalSize);
             console.log(details.name + ': ' + details.stats.minifiedSize);
         }))
-		.pipe(RENAME({suffix: '.min'}))
-		.pipe(GULP.dest('source/_prod/css'))
-		.pipe(GULP.dest('html/css'));
+        .pipe(RENAME({ suffix: '.min' }))
+        .pipe(GULP.dest('dist/css'))
+        .pipe(GULP.dest('html/css'));
 
-	GULP.src('source/pre/css/**/*.css')
-		.pipe(GULP.dest('source/_prod/css'))
-		.pipe(GULP.dest('html/css'));
-});
-
-GULP.task('sass-wp', function() {
-	GULP.src('source/pre/sass/**/*.sass')
-		.pipe(SASS.sync().on('error', SASS.logError))
-		.pipe(PREFIXCSS())
-		.pipe(MINCSS({debug: true}, function(details) {
+    GULP.src('source/sass/views/**/*.sass')
+        .pipe(SASS.sync().on('error', SASS.logError))
+        .pipe(PREFIXCSS())
+        .pipe(MINCSS({ debug: true }, function(details) {
             console.log(details.name + ': ' + details.stats.originalSize);
             console.log(details.name + ': ' + details.stats.minifiedSize);
         }))
-		.pipe(RENAME({suffix: '.min'}))
-		.pipe(GULP.dest('wordpress/wp-content/themes/new-theme/css'));
+        .pipe(RENAME({ suffix: '.min' }))
+        .pipe(GULP.dest('dist/views'))
+        .pipe(GULP.dest('html/views'));
 });
 
-// JAVASCRIPT
+// JavaScript
 ///////////////////////////////////////////////
 
-GULP.task('js-dev', function(){
-	GULP.src(['source/pre/js/**/*.js'])
-		.pipe(JSCONCAT('app.min.js'))
-		.pipe(GULP.dest('source/post/js'))
-		.pipe(GULP.dest('source/_dev/js'));
+GULP.task('js-dev', function() {
+    GULP.src(['source/pre/js/**/*.js'])
+        .pipe(JSCONCAT('app.min.js'))
+        .pipe(GULP.dest('source/post/js'))
+        .pipe(GULP.dest('source/_dev/js'));
 });
 
-GULP.task('js-prod', function(){
-	GULP.src(['source/js/**/*.js'])
-		.pipe(JSCONCAT('*.js'))
-		.pipe(JSUGLIFY())
-		.pipe(RENAME('app.min.js'))
-		.pipe(GULP.dest('source/_prod/js'))
-		.pipe(GULP.dest('html/js'));
+GULP.task('js-prod', function() {
+    GULP.src(['source/js/**/*.js'])
+        .pipe(JSCONCAT('*.js'))
+        .pipe(JSUGLIFY())
+        .pipe(RENAME('app.min.js'))
+        .pipe(GULP.dest('source/_prod/js'))
+        .pipe(GULP.dest('html/js'));
 });
 
-GULP.task('js-wp', function(){
-	GULP.src(['source/js/**/*.js'])
-		.pipe(JSCONCAT('*.js'))
-		.pipe(JSUGLIFY())
-		.pipe(RENAME('app.min.js'))
-		.pipe(GULP.dest('wordpress/wp-content/themes/new-theme/js'));
-});
-
-// IMAGES
+// Images
 ///////////////////////////////////////////////
 
-GULP.task('imagemin', function(){
-	GULP.src('source/pre/assets/images/**/*')
-		.pipe(IMAGEMIN())
-		.pipe(GULP.dest('source/_dev/assets/'))
-		.pipe(GULP.dest('html/assets/images/'));
+GULP.task('imagemin', function() {
+    GULP.src('source/pre/assets/images/**/*')
+        .pipe(IMAGEMIN())
+        .pipe(GULP.dest('source/_dev/assets/'))
+        .pipe(GULP.dest('html/assets/images/'));
 });
 
-GULP.task('imagemin-prod', function(){
-	GULP.src('source/pre/assets/images/**/*')
-		.pipe(IMAGEMIN())
-		.pipe(GULP.dest('source/_prod/assets/'))
-		.pipe(GULP.dest('html/assets/images/'));
-});
-
-GULP.task('imagemin-wp', function(){
-	GULP.src('source/pre/assets/images/**/*')
-		.pipe(IMAGEMIN())
-		.pipe(GULP.dest('wordpress/wp-content/themes/new-theme/assets'));
-});
-
-// FONTS
-///////////////////////////////////////////////
-
-GULP.task('fonts', function(){
-	GULP.src('source/pre/assets/fonts/**/*')
-		.pipe(GULP.dest('source/post/fonts/**/*'))
-		.pipe(GULP.dest('source/_dev/fonts/**/*'))
-		.pipe(GULP.dest('source/_prod/fonts/**/*'))
-		.pipe(GULP.dest('html/assets/fonts/**/*'));
-});
-
-GULP.task('fonts-wp', function(){
-	GULP.src('source/pre/assets/fonts/**/*')
-		.pipe(GULP.dest('wordpress/wp-content/themes/new-theme/fonts/**/*'));
+GULP.task('imagemin-prod', function() {
+    GULP.src('source/pre/assets/images/**/*')
+        .pipe(IMAGEMIN())
+        .pipe(GULP.dest('source/_prod/assets/'))
+        .pipe(GULP.dest('html/assets/images/'));
 });
